fbdd23cfdbd04bcf07f5d3a6d8e0453ecf227008
Jose <hurtadorivasj123@gmail.com>
2025-11-07 15:11:14 -0500

    AVANCE

--- src/app/pages/configuration/configuration.component.ts
+++ src/app/pages/configuration/configuration.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatCard } from "@angular/material/card";
+import { MatFormField, MatLabel } from "@angular/material/form-field";
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-configuration',
   standalone: true,
-  imports: [],
+  imports: [MatCard, MatFormField, MatLabel, MatIcon],
   templateUrl: './configuration.component.html',
   styleUrl: './configuration.component.scss'
 })
+
 export class ConfigurationComponent {
+
 
 }
